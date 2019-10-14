27f2682625bc31037953ee5234591d50d4527aaf
Iuliia <code.enemy@gmail.com>
2019-10-14 15:04:34 +0300

    9 - add redirection from login/register components when user is signed in

--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
+import firebase from './firebase'
 import 'semantic-ui-css/semantic.min.css'
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, withRouter } from 'react-router-dom'
 
 import App from './components/App'
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
 
-const Root = () => (
+class Root extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push('/')
+      }
+    })
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Switch>
+    )
+  }
+}
+const RootWithRouter = withRouter(Root)
+ReactDOM.render(
   <BrowserRouter>
-    <Switch>
-      <Route exact path='/' component={App} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/register' component={Register} />
-    </Switch>
-  </BrowserRouter>
+    <RootWithRouter />
+  </BrowserRouter>,
+  document.getElementById('root')
 )
-
-ReactDOM.render(<Root />, document.getElementById('root'))
 registerServiceWorker()
