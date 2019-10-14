2eec6c81aeb01b655abbba8ec131c1f8f2c89b0d
Iuliia <code.enemy@gmail.com>
2019-10-14 12:23:53 +0300

    Add form validation and save user information to the firebase database

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import firebase from '../../firebase'
+import md5 from 'md5'
 
 import { Link } from 'react-router-dom'
 import {
@@ -18,24 +19,115 @@ class Register extends Component {
     email: '',
     password: '',
     passwordConfirmation: '',
+    errors: [],
+    loading: false,
+    usersRef: firebase.database().ref('users'),
   }
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value })
   }
+  isFormValid = () => {
+    let errors = []
+    let error
+    if (this.isFormEmpty(this.state)) {
+      error = { message: 'Please, fill all fields' }
+      this.setState({ errors: errors.concat(error) })
+      return false
+    } else if (!this.isPasswordValid(this.state)) {
+      error = { message: 'Password is not valid' }
+      this.setState({ errors: errors.concat(error) })
+      return false
+    } else if (this.isEmailValid(this.state)) {
+      error = { message: 'Email is not valid' }
+      this.setState({ errors: errors.concat(error) })
+      return false
+    } else {
+      return true
+    }
+  }
+  isFormEmpty = ({ username, password, email, passwordConfirmation }) => {
+    return (
+      !username.length ||
+      !password.length ||
+      !email.length ||
+      !passwordConfirmation.length
+    )
+  }
+  isPasswordValid = ({ password, passwordConfirmation }) => {
+    if (password.length < 6 || passwordConfirmation.length < 6) {
+      return false
+    } else if (password !== passwordConfirmation) {
+      return false
+    } else {
+      return true
+    }
+  }
+  isEmailValid({ email }) {
+    const expRegMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i
+    return !expRegMail.test(email)
+  }
+  handleInputError = (errors, inputName) => {
+    return errors.some(error => error.message.toLowerCase().includes(inputName))
+      ? 'error'
+      : ''
+  }
+  displayErrors = errors => {
+    errors.map((error, i) => <p key={i}>{error.message}</p>)
+  }
   handleSubmit = e => {
     e.preventDefault()
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then(createdUser => {
-        console.log(createdUser)
-      })
-      .catch(err => {
-        console.log(err)
-      })
+    if (this.isFormValid()) {
+      this.setState({ errors: [], loading: true })
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(createdUser => {
+          console.log(createdUser)
+          createdUser.user
+            .updateProfile({
+              displayName: this.state.username,
+              photoURL: `http://gravatar.com/avatar/${md5(
+                createdUser.user.email
+              )}?d=identicon`,
+            })
+            .then(() => {
+              this.saveUser(createdUser).then(() => {
+                console.log('user saved')
+              })
+            })
+            .catch(err => {
+              console.log(err)
+              this.setState({
+                errors: this.state.errors.concat(err),
+                loading: false,
+              })
+            })
+        })
+
+        .catch(err => {
+          console.log(err)
+          this.setState({
+            errors: this.state.errors.concat(err),
+            loading: false,
+          })
+        })
+    }
+  }
+  saveUser = createdUser => {
+    return this.state.usersRef.child(createdUser.user.uid).set({
+      name: createdUser.user.displayName,
+      avatar: createdUser.user.photoURL,
+    })
   }
   render() {
-    const { username, email, password, passwordConfirmation } = this.state
+    const {
+      username,
+      email,
+      password,
+      passwordConfirmation,
+      errors,
+      loading,
+    } = this.state
     return (
       <Grid textAlign='center' verticalAlign='middle' className='app'>
         <Grid.Column style={{ maxWidth: 450 }}>
@@ -52,6 +144,7 @@ class Register extends Component {
                 iconPosition='left'
                 placeholder='Username'
                 onChange={this.handleChange}
+                className={this.handleInputError(errors, 'username')}
                 type='text'
                 value={username}
               />
@@ -62,6 +155,7 @@ class Register extends Component {
                 iconPosition='left'
                 placeholder='Email'
                 onChange={this.handleChange}
+                className={this.handleInputError(errors, 'email')}
                 type='email'
                 value={email}
               />
@@ -72,6 +166,7 @@ class Register extends Component {
                 iconPosition='left'
                 placeholder='Password'
                 onChange={this.handleChange}
+                className={this.handleInputError(errors, 'password')}
                 type='password'
                 value={password}
               />
@@ -82,14 +177,27 @@ class Register extends Component {
                 iconPosition='left'
                 placeholder='Confirm your password'
                 onChange={this.handleChange}
+                className={this.handleInputError(errors, 'password')}
                 type='password'
                 value={passwordConfirmation}
               />
-              <Button color='orange' fluid size='large'>
+              <Button
+                disabled={loading}
+                className={loading ? 'loading' : ''}
+                color='orange'
+                fluid
+                size='large'
+              >
                 Sign Up
               </Button>
             </Segment>
           </Form>
+          {this.state.errors > 0 && (
+            <Message error>
+              <h4>Error</h4>
+              {this.displayErrors(errors)}
+            </Message>
+          )}
           <Message>
             Already have an account? <Link to='/login'>Sign In</Link>
           </Message>
